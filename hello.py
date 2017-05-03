8a30d01e70bad8adde01931b07a4dcb39211c230
Jeffrey <jeffrey.kimmel@outlook.com>
2017-05-02 20:07:11 -0400

    Added started of Greeter class

--- hello.py
+++ hello.py
@@ -1,13 +1,3 @@
 print("Hello, GitHub!")
 
 
-class Greeter:
-
-    def __init__(self, message):
-        self.message = message
-
-    def greeting(self, message):
-        print(self.message)
-
-greeter = Greeter("Hello from greeter!")
-greeter.greeting()
